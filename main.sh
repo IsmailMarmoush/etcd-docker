fa054254ea855737892ba0004d472ce38e331fc1
Ismail Marmoush <ismail.marmoush@zalando.de>
2018-12-12 21:29:33 +0100

    before routine

--- main.sh
+++ main.sh
@@ -2,11 +2,28 @@
 host=http://localhost:32770/v2
 
 put(){
-	curl $host/keys/"$1" -XPUT -d value="$2" | jq
+	curl $host/keys"$1" -XPUT -d value="$2" | jq
 }
 
+
 get(){
-	curl $host/keys/$1 | jq
+	curl $host/keys$1 | jq
+}
+
+delete(){
+  curl "$host/keys"$1"?recursive=true" -XDELETE | jq
+}
+
+put_dir(){
+  curl $host/keys"$1" -XPUT -d dir=true | jq
+}
+
+get_dir(){
+  curl  $host/keys"$1"?recursive=true | jq
+}
+
+delete_dir(){
+  curl "$host/keys"$1"?dir=true&recursive=true" -XDELETE | jq
 }
 
 $@
