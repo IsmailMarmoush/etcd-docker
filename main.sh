33ced6f882cc395f9691b2df811cb993f3a5b249
Ismail Marmoush <marmoushismail@gmail.com>
2020-03-05 13:40:05 +0200

    format and better api

--- main.sh
+++ main.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
-host=http://localhost:32770/v2
+host=http://localhost:9001/v2
 
-put(){
-  IN=$1
-  arr=(${IN//=/ })
-  k=${arr[0]}
-  v=${arr[1]}
-	curl $host/keys"$k" -XPUT -d value="$v" | jq
+put() {
+  k=$1
+  v=$2
+  curl $host/keys/"$k" -XPUT -d value="$v" | jq
 }
 
-get(){
-	curl $host/keys$1?recursive=true | jq
+get() {
+  curl $host/keys$1?recursive=true | jq
 }
 
-delete(){
+delete() {
   curl "$host/keys"$1"?recursive=true" -XDELETE | jq
 }
 
-put_dir(){
+put_dir() {
   curl $host/keys"$1" -XPUT -d dir=true | jq
 }
 
-get_dir(){
-  curl  $host/keys"$1"?recursive=true | jq
+get_dir() {
+  curl $host/keys"$1"?recursive=true | jq
 }
 
-delete_dir(){
+delete_dir() {
   curl "$host/keys"$1"?dir=true&recursive=true" -XDELETE | jq
 }
 
